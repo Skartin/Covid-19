e90968ed57c44acbe56ef67335f9cb2bd3c9ec13
Saeid Khademi <s_khademi@ymail.com>
2024-04-30 23:29:23 -0700

    Update Covid19.sql

--- SQLPortfolio.sql
+++ SQLPortfolio.sql
@@ -8,7 +8,7 @@ Skills used: Joins, Aggregate Functions, Creating Views, Converting Data Types
 
 -- Total Cases vs Total Deaths vs total vaccinations
 Select Location, date, total_cases, total_vaccinations
-From SQLPortfolio..CovidDeaths
+From covid-19..CovidDeaths
 Where continent is not null 
 order by 1,2
 
@@ -27,13 +27,13 @@ ORDER BY
 
 -- Total Cases vs Population
 Select Location, date, Population, total_cases
-From SQLPortfolio..CovidDeaths
+From covid-19..CovidDeaths
 order by 1,2
 
 
 -- Countries with Lowest Death Count per Population
 Select Location, MIN(cast(Total_deaths as int)) as TotalDeathCount
-From SQLPortfolio..CovidDeaths
+From covid-19..CovidDeaths
 --Where location like '%states%'
 Where continent is not null 
 Group by Location
@@ -41,7 +41,7 @@ order by TotalDeathCount desc
 
 -- Showing contintents with the highest death count per population
 Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
-From SQLPortfolio..CovidDeaths
+From covid-19..CovidDeaths
 Where continent is not null 
 Group by continent
 order by TotalDeathCount desc
@@ -51,8 +51,8 @@ order by TotalDeathCount desc
 Create View PercentPopulationVaccinated as
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
-From SQLPortfolio..CovidDeaths dea
-Join SQLPortfolio..CovidVaccinations vac
+From covid-19..CovidDeaths dea
+Join covid-19..CovidVaccinations vac
 	On dea.location = vac.location
 	and dea.date = vac.date
 where dea.continent is not null 
@@ -60,7 +60,7 @@ where dea.continent is not null
 	
 -- GLOBAL NUMBERS
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
-From SQLPortfolio..CovidDeaths
+From covid-19..CovidDeaths
 --Where location like '%states%'
 where continent is not null 
 --Group By date
