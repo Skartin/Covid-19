4902ea1030d2b53072d568aa7e97059d099d33f5
Nick S. Khademi <s_khademi@ymail.com>
2024-05-30 11:44:44 -0700

    Update Covid19.sql

--- Covid19.sql
+++ Covid19.sql
@@ -1,17 +1,20 @@
-/*
+-- Covid 19 Data Exploration -- Skills used: Joins, Aggregate Functions, Creating Views, Converting Data Types
 
-Covid 19 Data Exploration 
-
-Skills used: Joins, Aggregate Functions, Creating Views, Converting Data Types
-
-*/
 
 -- Total Cases vs Total Deaths vs total vaccinations
-Select Location, date, total_cases, total_vaccinations
-From covid-19..CovidDeaths
-Where continent is not null 
-order by 1,2
+SELECT 
+    Location,
+    date,
+    total_cases,
+    total_vaccinations
+FROM 
+    covid-19..CovidDeaths
+WHERE 
+    continent IS NOT NULL
+ORDER BY 
+    1, 2;
 
+---------------------------------------------------------------
 -- Query to retrieve daily new cases for The United States:
 SELECT DISTINCT
     date,
@@ -23,69 +26,99 @@ WHERE
 ORDER BY 
     date;
 
-
-
+---------------------------------------------------------------
 -- Total Cases vs Population
-Select Location, date, Population, total_cases
-From covid-19..CovidDeaths
-order by 1,2
+SELECT 
+    Location,
+    Date,
+    Population,
+    total_cases
+FROM 
+    covid-19..CovidDeaths
+ORDER BY 
+    1, 2;
 
-
+---------------------------------------------------------------
 -- Countries with Lowest Death Count per Population
-Select Location, MIN(cast(Total_deaths as int)) as TotalDeathCount
-From covid-19..CovidDeaths
---Where location like '%states%'
-Where continent is not null 
-Group by Location
-order by TotalDeathCount desc
+SELECT 
+    Location,
+    MIN(CAST(Total_deaths AS INT)) AS TotalDeathCount
+FROM 
+    covid-19..CovidDeaths
+WHERE 
+    continent IS NOT NULL
+GROUP BY 
+    Location
+ORDER BY 
+    TotalDeathCount DESC;
 
+---------------------------------------------------------------
 -- Showing contintents with the highest death count per population
-Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
-From covid-19..CovidDeaths
-Where continent is not null 
-Group by continent
-order by TotalDeathCount desc
+SELECT 
+    continent,
+    MAX(CAST(Total_deaths AS INT)) AS TotalDeathCount
+FROM 
+    covid-19..CovidDeaths
+WHERE 
+    continent IS NOT NULL
+GROUP BY 
+    continent
+ORDER BY 
+    TotalDeathCount DESC;
 
-
+---------------------------------------------------------------
 -- Creating View to store data for visualizations
-Create View PercentPopulationVaccinated as
-Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
-, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
-From covid-19..CovidDeaths dea
-Join covid-19..CovidVaccinations vac
-	On dea.location = vac.location
-	and dea.date = vac.date
-where dea.continent is not null 
+CREATE VIEW PercentPopulationVaccinated AS
+SELECT 
+    dea.continent,
+    dea.location,
+    dea.date,
+    dea.population,
+    vac.new_vaccinations,
+    SUM(CONVERT(INT, vac.new_vaccinations)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS RollingPeopleVaccinated
+FROM 
+    covid-19..CovidDeaths dea
+JOIN 
+    covid-19..CovidVaccinations vac ON dea.location = vac.location AND dea.date = vac.date
+WHERE 
+    dea.continent IS NOT NULL;
 
-	
+---------------------------------------------------------------
 -- GLOBAL NUMBERS
-Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
-From covid-19..CovidDeaths
---Where location like '%states%'
-where continent is not null 
---Group By date
-order by 1,2
+SELECT 
+    SUM(new_cases) AS total_cases,
+    SUM(CAST(new_deaths AS INT)) AS total_deaths,
+    SUM(CAST(new_deaths AS INT)) / SUM(new_cases) * 100 AS DeathPercentage
+FROM 
+    covid-19..CovidDeaths
+WHERE 
+    continent IS NOT NULL
+ORDER BY 
+    1, 2;
 
-
+---------------------------------------------------------------
 --Retrieve the total deaths by country
 SELECT 
     d.location AS country,
-    sum(d.new_deaths)AS total_deaths
+    SUM(d.new_deaths) AS total_deaths
 FROM 
     coviddeaths d
 WHERE 
     d.continent IS NOT NULL 
-GROUP by d.location
+GROUP BY 
+    d.location;
 
-
-
+---------------------------------------------------------------
 -- Total Population vs Vaccinations by Country
 SELECT
-	vac.continent AS Continent, 
-	vac.location AS Country, 
-	vac.total_vaccinations AS Vaccination
+    vac.continent AS Continent,
+    vac.location AS Country,
+    vac.total_vaccinations AS Vaccination
 FROM 
-	CovidVaccinations vac
+    CovidVaccinations vac
 WHERE 
-	vac.continent IS NOT NULL  AND vac.date IS "4/30/21"
-	GROUP by vac.location
+    vac.continent IS NOT NULL  
+    AND vac.date = '4/30/21'
+GROUP BY 
+    vac.location;
+
